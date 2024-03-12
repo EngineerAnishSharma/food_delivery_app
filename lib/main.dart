00392c5eb87efc50aea6bfb6c9eb8b29a780b0d0
Anish <engineeranish10000@gmail.com>
2024-03-13 00:01:57 +0530

    admin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
-import 'package:food_delivery_app/pages/onboard.dart';
+import 'package:food_delivery_app/admin/admin_home.dart';
 import 'package:food_delivery_app/widgets/app_constant.dart';
 
 void main() async {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Onboard(),
+      home: const HomeAdmin(),
     );
   }
 }
